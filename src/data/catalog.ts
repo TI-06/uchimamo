5a26e907e565131997c8739645a90a9343478004
TI-06 <iretaku.shop@gmail.com>
2026-08-19 23:24:55 +0900

    refactor: keep one product catalog merge path

--- src/data/catalog.ts
+++
@@ -1,7 +0,0 @@
-import baseProductsJson from './products.json';
-import extraProductsJson from './products-extra.json';
-import type { Product } from '../types/product';
-
-export const products = [...baseProductsJson, ...extraProductsJson] as Product[];
-
-export default products;
